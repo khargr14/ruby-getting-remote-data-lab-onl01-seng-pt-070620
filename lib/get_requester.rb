4f3f68215d73f5d5fea0e74dceec3c355683b46e
Keesha Hargrove <kharg14@gmail.com>
2020-08-13 10:47:01 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,4 +1,9 @@
 # Write your code here
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+ 
 class GetRequester
   
  url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
