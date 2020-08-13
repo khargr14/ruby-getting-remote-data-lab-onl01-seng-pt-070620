0f2d0212a0dc9a9cb434087c77734de7da44a75a
Keesha Hargrove <kharg14@gmail.com>
2020-08-13 10:43:15 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -12,3 +12,16 @@ class GetRequester
 end
 
 
+URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+ 
+  def get_programs
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+ 
+end
+ 
+programs = GetPrograms.new.get_programs
+puts programs
+ 
