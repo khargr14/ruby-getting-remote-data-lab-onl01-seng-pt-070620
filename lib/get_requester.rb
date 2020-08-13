eedd9f0f8367fbe06570d6c5becbbb7b00383638
Keesha Hargrove <kharg14@gmail.com>
2020-08-13 10:45:57 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,9 +4,11 @@ class GetRequester
  url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
   def get_requester
-    
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
   
-  
+end
   
   
   
